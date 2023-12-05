84bb3e1bf3bc6545e0108ceb4fc1c33b5de18967
Mercia Marques <mercia.marques@xpi.com.br>
2023-12-04 23:12:08 -0300

    creating Stock view

---
+++ NovaMoedaInvestimentos/Controllers/StockController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NovaMoedaInvestimentos.Repositories.Interfaces;
+
+namespace NovaMoedaInvestimentos.Controllers
+{
+    public class StockController : Controller
+    {
+        private readonly IStockRepository _stockRepository;
+
+        public StockController(IStockRepository stockRepository)
+        {
+            _stockRepository = stockRepository;
+        }
+        public IActionResult List()
+        {
+            var stocks = _stockRepository.Stocks;
+            return View(stocks);
+        }
+    }
+}
